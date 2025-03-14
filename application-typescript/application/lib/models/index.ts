ed6cdeeb77e4e682167da5cc9ca09473f8b77d89
eferos <eros.fabrici@gmail.com>
2025-03-14 11:50:16 +0000

    got rid of old api

--- application-typescript/application/lib/models/index.ts
+++
@@ -1,6 +0,0 @@
-import { model as UserModel } from './user.model';
-import { model as ProductModel } from './product.model';
-import { model as AgreementModel } from './agreement.model';
-import { model as InventoryModel } from './inventory.model';
-
-export { UserModel, ProductModel, AgreementModel, InventoryModel };
--- application-typescript/clean.sh
+++
@@ -1,7 +0,0 @@
-rm -rf wallet
-rm -rf identities
-rm nextblock
-rm -rf ./*Registrar
-
-# node db_handler.js drop
-rm -rf *.log
