57aa5760653585f907559b312bc975bfa404fc86
eferos <eros.fabrici@gmail.com>
2025-12-18 16:23:02 +0000

    small bug fix, sending the correct genesis block

--- scripts/createChannel.sh
+++ scripts/createChannel.sh
@@ -40,14 +40,14 @@ createChannelA() {
         if [ "$STAGE" == "prod" ] && [ "$org" == "$REMOTE_ORG" ]; then
              printInfo "Joining Channel on REMOTE $org..."
              # Sync genesis block
-             rsync -azP system-genesis-block ${REMOTE_SSH}:${REMOTE_FABRIC_HOME}/
+             rsync -azP channel-artifacts ${REMOTE_SSH}:${REMOTE_FABRIC_HOME}/
              
              ssh ${REMOTE_SSH} "cd ${REMOTE_FABRIC_HOME} && sudo ./peer.sh joinchannel -n peer0.$org.dt4h.com -A"
              ssh ${REMOTE_SSH} "cd ${REMOTE_FABRIC_HOME} && sudo ./peer.sh joinchannel -n peer1.$org.dt4h.com -A"
 
              # Sync genesis block back to local
              printInfo "Syncing genesis block back from REMOTE..."
-             rsync -azP ${REMOTE_SSH}:${REMOTE_FABRIC_HOME}/system-genesis-block/ system-genesis-block/
+             rsync -azP ${REMOTE_SSH}:${REMOTE_FABRIC_HOME}/channel-artifacts/ channel-artifacts/
         else
 		    ./peer.sh joinchannel -n peer0."$org".dt4h.com -A 
 		    ./peer.sh joinchannel -n peer1."$org".dt4h.com -A 
